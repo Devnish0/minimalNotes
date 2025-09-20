b5b07c3a28ba902e224219707840944236143c30
nishhhhh <156004996+Devnish0@users.noreply.github.com>
2025-09-20 23:08:00 +0530

    starting

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const notes = document.getElementById("notes");
 const conttxt = document.getElementById("container1");
 const contmain = document.getElementById("container2");
 const maindate = document.getElementById("date1");
+const sbtBtn = document.getElementById("smt");
+const headText = document.getElementById("headtext"); // Title input
+const textBody = document.getElementById("bodytext"); // Textarea content
 
 function createNote(title, time, text) {
   let color = [`#CBA9FF`, `#D4FF6A`, `#AEE1F9`, `#FFD3A5`, `yellow`];
@@ -20,16 +23,25 @@ function createNote(title, time, text) {
   conttxt.className = "addNoteContainer";
   contmain.className = "hidden";
   maindate.innerText = `${din}th ${mahina} ${ghante}`;
-
-  note.classList.add("note");
-  note.style.backgroundColor = color[randomColor];
-  note.innerHTML = `
-                <span class="title">${title}</span>
-                <span class="date">${din}th ${mahina} ${ghante}</span>
-                <span class="text">${text}</span>`;
-  notes.append(note);
-  s;
+  // headtext
+  //   note.classList.add("note");
+  //   note.style.backgroundColor = color[randomColor];
+  //   note.innerHTML = `
+  //                 <span class="title">${title}</span>
+  //                 <span class="date">${din}th ${mahina} ${ghante}</span>
+  //                 <span class="text">${text}</span>`;
+  //   notes.append(note);
+  //   s;
 }
+function saveTheValue(params) {
+  let title = headText.value; // Gets the title from input
+  let text = textBody.value; // Gets the content from textarea
+  console.log("Title:", title);
+  console.log("Content:", text);
+}
+sbtBtn.addEventListener("click", () => {
+  saveTheValue();
+});
 addBtn.addEventListener("click", () => {
-  createNote("nishank", "23:32", "this is the test");
+  createNote();
 });
