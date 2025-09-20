3a0c327ef3cc4372733fcb0f7198f9dffc6bbb62
nishhhhh <156004996+Devnish0@users.noreply.github.com>
2025-09-20 15:52:12 +0530

    did some more

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const addBtn = document.getElementById('add');
-const body = document.body
-const notes = document.getElementById("notes")
+const addBtn = document.getElementById("add");
+const body = document.body;
+const notes = document.getElementById("notes");
+const conttxt = document.getElementById("container1");
+const contmain = document.getElementById("container2");
+const maindate = document.getElementById("date1");
 
-function createNote(title,time,text) {
-    let color = [`#CBA9FF`,`#D4FF6A`,`#AEE1F9`,`#FFD3A5`,`yellow`]
-    let randomColor = Math.floor((Math.random()*color.length))
-    const note  = document.createElement('div');
-    let samay = new Date();
-    let din = samay.getDay()
-    let mahina = samay.toLocaleString('default',{month:'short'})
-    let ghante = samay.toLocaleString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-    });
+function createNote(title, time, text) {
+  let color = [`#CBA9FF`, `#D4FF6A`, `#AEE1F9`, `#FFD3A5`, `yellow`];
+  let randomColor = Math.floor(Math.random() * color.length);
+  const note = document.createElement("div");
+  let samay = new Date();
+  let din = samay.getDay();
+  let mahina = samay.toLocaleString("default", { month: "short" });
+  let ghante = samay.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  conttxt.className = "addNoteContainer";
+  contmain.className = "hidden";
+  maindate.innerText = `${din}th ${mahina} ${ghante}`;
 
-    note.classList.add('note');
-    note.style.backgroundColor = color[randomColor]
-    note.innerHTML = `
+  note.classList.add("note");
+  note.style.backgroundColor = color[randomColor];
+  note.innerHTML = `
                 <span class="title">${title}</span>
                 <span class="date">${din}th ${mahina} ${ghante}</span>
-                <span class="text">${text}</span>`
-    notes.append(note)
-
+                <span class="text">${text}</span>`;
+  notes.append(note);
+  s;
 }
-addBtn.addEventListener('click',()=>{ createNote("nishank","23:32", "this is the test")});
+addBtn.addEventListener("click", () => {
+  createNote("nishank", "23:32", "this is the test");
+});
